55e437d629a6293c0eea95d0164a008ad34e545b
feuray <fyfeufeu@gmail.com>
2023-09-29 00:20:17 +0200

    page carte js article

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,230 +1,138 @@
-//556 lignes ------- 413 arrivé 
+//867 lignes -------  343 arrivé 
 //=============================== modele html ====================================================//
 fetch('http://localhost:3000/api/products')//je récupére les données
   .then(data => data.json())//je les appel data et les converties en jason
   .then(data => {//j'appel mon objet card et lui aplique la fonction qui suit
-    //les données du fetch
-    /*
-    console.log(data);//tout
-    console.log(data.length);//8      
-    console.log(data[0]);        
-    console.log(data[0]["_id"]);//ok
-    console.log(data[0]["colors"][0]);//
-    console.log(data[0]["colors"][1]);//
-    console.log(data[0]["colors"][2]);//
-  
-    console.log(data[0]["altTxt"]);//
-    console.log(data[0]["description"]);//
-    console.log(data[0]["name"]);//
-    console.log(data[0]["price"]);//
-    console.log(data[0]["imageUrl"]);*/
 
-    //console.log(data[0]["_id"]);//ok
-
-
-    //les donnees du local storage
-    //let produitLocalStorage = JSON.parse(localStorage.getItem("panier"));//récupère la valeur par clé.
-    //console.log(produitLocalStorage);//tout
-
-    /*const cherche = Object.entries(produitLocalStorage);
-    const reCherche = Object.entries(cherche[0][1]);
-
-    let localId = cherche[0][0];
-    let localColor = reCherche[0][0];
-    let localQt = reCherche[0][1];
-
-    console.log(cherche);//array
-    console.log(cherche[0]);//array
-
-    console.log(cherche[0][0]);//id de array 0
-    console.log(reCherche[0][0]);//qt de array 0
-    console.log(reCherche[0][1]);//1 qt de array 0
-
-    console.log(localId);//id de array 0
-    console.log(localColor);//qt de array 0
-    console.log(localQt);//1 qt de array 0*/
-
-
-  //}
-});//fetch
-
-
-    /*
-    const arrta = Object.entries(produitLocalStorage);
-    console.log(arrta);
-    
-    console.log(arrta[0][0]);//0k id
-    console.log(arrta[0][1]);//objet Blue 1
-    
-    console.log((arrta[0][1]).keys);//objet Blue 1
-    
-    
-    console.log(arrta[0][1]["Blue"]);//qt 1*/
-    //ok ca marche id*/
     
     const produitLocalStorage = JSON.parse(localStorage.getItem("panier"));
-    
+
+    let Qt = "";
+    let prix = 0;
+    console.log(Qt);
+    console.log(prix);
+
     for (const [key, value] of Object.entries(produitLocalStorage)) {
       //console.log(`${key}: ${value}`);
-      console.log(`${key}`);
+      //console.log(`${key}`);
       //console.log(`${value}`);
 
       fetch("http://localhost:3000/api/products/" + key)
       .then((response) => response.json())
       .then(product => {
-        console.log(product);
-        console.log(product.imageUrl);
+      console.log(product.imageUrl);//adresse image
+        
+      for (const [cle, valeur] of Object.entries(value)) {
+      //console.log(`${cle}: ${valeur}`);
+      //colors qt price
+        console.log(`${cle}`);//color blue ok
+        console.log(`${valeur}`);//quantité ok
+      
 
-        //console.log(product[key].imageUrl;
-        let monPanier = document.getElementById("cart__items");
-        let productArticle = document.createElement("article");
+      
+      let cart__item = document.getElementById("cart__items");
+
+        let productArticle = document.createElement("article");//1 declinaison
         productArticle.classList.add("cart__item");
+        cart__item.appendChild(productArticle);
+        
         //productArticle.setAttribute('id', data[0]["_id"]);
         //productArticle.setAttribute('data', 'color');
-        monPanier.appendChild(productArticle);
+        cart__item.appendChild(productArticle);
 
-       //
-        let div_image = document.createElement("div");
+       
+        let div_image = document.createElement("div");//2
         div_image.classList.add("cart__item__img");
         productArticle.appendChild(div_image);
 
+        
+
         let image = document.createElement("img");
-        image.src = product.imageUrl;//data[i].imageUrl;
-        image.alt = product.alttxt;"Photographie d'un canapé";//data[i].altTxt;
+        image.src = product.imageUrl;
+        image.alt = product.altTxt;
         div_image.appendChild(image);
-
-
-
-        //2
-        let div_content = document.createElement("div");
-        div_content.classList.add("cart__item__content");
-        productArticle.appendChild(div_content);
-
-
-    
-       let div_description = document.createElement("div");
-        div_description.classList.add("cart__item__content__description");
-        div_content.appendChild(div_description);
-
-    
-      let descript_h = document.createElement("h2");
-      descript_h.innerHTML = product.name;
-      div_description.appendChild(descript_h);
-
-        console.log(product.price);
-        let totpx = product.price;
-        console.log(totpx);
-
-        for (const [cle, valeur] of Object.entries(value)) {
-          console.log(`${cle}: ${valeur}`);
-        //injection 118 blue 1 et 118 black/YELLOW 2
-          //3
-          //console.log(`${cle}: ${valeur}`);
-          console.log(`${cle}`);
-          console.log(`${valeur}`);
-          let descript_p = document.createElement("p");
-          descript_p.innerHTML = `${cle}`;       //"Vert";
-          div_description.appendChild(descript_p);
-          let descript_2p = document.createElement("p");
-          descript_2p.innerHTML = `${valeur}` * totpx;
-          div_description.appendChild(descript_2p);
       
-          //4
-          let div_settings = document.createElement("div");
-          div_settings.classList.add("cart__item__content__settings");
-          div_content.appendChild(div_settings);
       
-          
-          let cart_quantiti = document.createElement("div");
-          cart_quantiti.classList.add("cart__item__content__settings__quantity");
-          div_settings.appendChild(cart_quantiti);
       
-          
-          let quanti_p = document.createElement("p");
-          quanti_p.innerHTML = "Qté : "
-          cart_quantiti.appendChild(quanti_p);
+
+
+
       
+      let div_content = document.createElement("div");//3
+      div_content.classList.add("cart__item__content");
+      productArticle.appendChild(div_content);
           
-          let quanti_input = document.createElement("input");
-          cart_quantiti.appendChild(quanti_input);
-          quanti_input.type = "number";//
-          quanti_input.classList.add("itemQuantity");// class="itemQuantity" name="itemQuantity"
-          quanti_input.name = "itemQuantity";// name="itemQuantity"
-          quanti_input.min = 1;//min max 1 100
-          quanti_input.max = 100;//min max 1 100
-          quanti_input.value = `${valeur}`;//value 42
+
+  
+     let div_description = document.createElement("div");//4
+      div_description.classList.add("cart__item__content__description");
+      div_content.appendChild(div_description);
+          //<div class="cart__item__content__description">
+
+
+          let descript_h = document.createElement("h2");
+          descript_h.innerHTML = product.name;
+          div_description.appendChild(descript_h);
+            
+            //2
+            let prix = product.price;
+            let descript_p = document.createElement("p");
+                descript_p.innerHTML = `${cle}`;       //"Vert";
+                div_description.appendChild(descript_p);
+                let descript_2p = document.createElement("p");
+                descript_2p.innerHTML = `${valeur}` * prix;//*`${valeur}`= qt
+                div_description.appendChild(descript_2p);
+                //console.log(product.price);
+
+
+      let div_settings = document.createElement("div");//
+      div_settings.classList.add("cart__item__content__settings");
+      div_content.appendChild(div_settings);
+
+      let cart_quantiti = document.createElement("div");
+      cart_quantiti.classList.add("cart__item__content__settings__quantity");
+      div_settings.appendChild(cart_quantiti);
+
+
+
+      let quanti_input = document.createElement("input");
+      quanti_input.type = "number";//
+      quanti_input.classList.add("itemQuantity");// class="itemQuantity" name="itemQuantity"
+      quanti_input.name = "itemQuantity";// name="itemQuantity"
+      quanti_input.min = 1;//min max 1 100
+      quanti_input.max = 100;//min max 1 100
+      quanti_input.value = `${valeur}`;//value 42
+      cart_quantiti.appendChild(quanti_input);
+      //console.log(quanti_input.value);//
+
+
+
+
       
-          
-          let cart_supp = document.createElement("div");
-          cart_supp.classList.add("cart__item__content__settings__delete");
-          div_settings.appendChild(cart_supp);
-          
+      let cart_supp = document.createElement("div");
+      cart_supp.classList.add("cart__item__content__settings__delete");
+      div_settings.appendChild(cart_supp);
+
           //bouton p class deleteItem
           let cart_delet = document.createElement("p");
           cart_delet.classList.add("deleteItem");
           cart_delet.innerHTML = "Supprimer";
           cart_supp.appendChild(cart_delet);
 
-          let total = document.getElementById("totalPrice");
-          total.innerHTML = product.price;//
-          console.log(product.price);
-
+    let totalQt = document.getElementById("totalQuantity");
+          totalQt.innerHTML = "qt";
+          let totalPrix = document.getElementById("totalPrice");
+          totalPrix.innerHTML = "px";
       
       }//2 for
-        });//fetch
-             }//1 for
-
-
-
-
+      });//fetch
+      }//1 for
+      });//fetch
 
 
 //https://stackoverflow.com/questions/2385701/regular-expression-for-first-and-last-name
 
-    /*
-    <div class="cart__item__content__settings__quantity">
-    <p>Qté : </p>
-    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-    </div>
-    
-    <div class="cart__item__content__settings__delete">
-    <p class="deleteItem">Supprimer</p>
-    </div>
-    
-    */
-    /*
-    //console.log(produitLocalStorage.keys);//
-      /*console.log(data[0]["_id"]);//ok id
-    
-    
-      console.log(produitLocalStorage["107fb5b75607497b96722bda5b504926"]);//Blue 1
-      console.log(produitLocalStorage["107fb5b75607497b96722bda5b504926"]["Blue"]);//qt1
-      
-      console.log(produitLocalStorage["107fb5b75607497b96722bda5b504926"].Blue);//1
-    
-    for (const prop in produitLocalStorage) {
-    console.log(prop, produitLocalStorage[prop]);
-    }
-    
-    const arrta = Object.entries(produitLocalStorage);
-    console.log(arrta);
-    
-    console.log(arrta[0][0]);//0k id
-    console.log(arrta[0][1]);//objet Blue 1
-    
-    console.log((arrta[0][1]).keys);//objet Blue 1
-    
-    
-    console.log(arrta[0][1]["Blue"]);//qt 1*/
-
-
-    /*
-    const arrtata = Object.entries(arrta);
-    console.log(arrtata);
-    console.log(arrtata[0][1][0]);*/
-
-
-
+   
 /*
 Sur cette page, l’utilisateur va pouvoir modifier la quantité d’un produit de son panier ; à ce
 moment, le total du panier devra bien se mettre à jour.
@@ -247,8 +155,12 @@ sont pas sécurisées et l’utilisateur pourrait alors modifier le prix lui-mê
 ///https://stackoverflow.com/questions/2385701/regular-expression-for-first-and-last-name
 // regex
 //function filtrer() {
-  /*let errors = "Erreur veuillez modifiez la saisie";
+  //let errors = "Erreur veuillez modifiez la saisie";
 
+
+//===================================================================
+// REGEX OK
+/*
   let  docprenom = document.getElementById("firstName").value;//saisie
   let  docnom = document.getElementById("lastName").value;
   let  docadresse = document.getElementById("address").value;
@@ -261,11 +173,10 @@ sont pas sécurisées et l’utilisateur pourrait alors modifier le prix lui-mê
   let  docvilleerr = document.getElementById("cityErrorMsg");
   let  docemailerr = document.getElementById("emailErrorMsg");
   
-  let expprenom = /^[A-Za-z \-éè^ç¨ïëî]+$/;//regex
-  let expnom = /^[A-Za-z \-éè^ç¨ïëî]/;
-  let expadresse = /^[A-Za-z0-9 -]+$/;
+  let expprenom = /^[^0-9]+$/;//tout sauf chiffres       tilde &#126; | ~
+  let expadresse = /^[A-Za-z0-9 -éèçïëî]+$/;//"^[^0-9]+$"
   let expville = /^[A-Za-z -]+$/;
-  let expemail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,6})+$/;
+  let expemail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/;//
 
 console.log(expprenom.test(docprenom));//true false
 console.log(expnom.test(docnom));//true false
@@ -280,17 +191,8 @@ if (expadresse.test(docadresse) === true) {} else {docadresseerr.innerHTML = err
 if (expville.test(docville) === true) {} else {docvilleerr.innerHTML = errors;}
 if (expemail.test(docemail) === true) {} else {docemailerr.innerHTML = errors;}
 */
-
-
-
-
-
-
-
-
-
-
-
+//======================= FIN REGEX OK================================================
+//=======================================================================
 /*     IMPORTANT
 let tutu = data[2]["colors"]["1"]; POUR ATTRAPPER TABLEAU DANS TABLEAU
 console.log(tutu);
@@ -411,3 +313,31 @@ Si un produit est ajouté dans le panier à plusieurs reprises, mais avec des co
 différentes, il doit apparaître en deux lignes distinctes avec la couleur et la quantité
 correspondantes indiquées à chaque fois
 */
+
+
+/*
+Validation des données
+Pour les routes POST, l’objet contact envoyé au serveur doit contenir les champs firstName,
+lastName, address, city et email. Le tableau des produits envoyé au back-end doit être un
+array de strings product-ID. Les types de ces champs et leur présence doivent être validés
+avant l’envoi des données au serveur.
+
+5 champs firstName .... array de string product-ID
+
+*/
+
+//requete fetch post stop page controle form data redirection
+/*
+/**
+ *
+ * Expects request to contain:
+ * contact: {
+ *   firstName: string,
+ *   lastName: string,
+ *   address: string,
+ *   city: string,
+ *   email: string
+ * }
+ * products: [string] <-- array of product _id
+ *
+ */
