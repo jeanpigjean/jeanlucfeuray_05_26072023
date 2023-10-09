f1d54c0e965b8240498c84f3dc149f420b39f6f0
feuray <fyfeufeu@gmail.com>
2023-10-09 21:19:09 +0200

    fait nomage variable etinput even changement html ok faire localstorage changement even

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,4 @@
-//867 lignes -------  152 arrivé 
+//867 lignes -------  255
 //=============================== modele html ====================================================//
 fetch('http://localhost:3000/api/products')//je récupére les données
   .then(data => data.json())//je les appel data et les converties en jason
@@ -52,24 +52,19 @@ fetch('http://localhost:3000/api/products')//je récupére les données
             div_content.appendChild(div_description);
             //<div class="cart__item__content__description">
 
-            let descript_h = document.createElement("h2");
-            descript_h.innerHTML = product.name;
-            div_description.appendChild(descript_h);
+            let descriptTitre = document.createElement("h2");//descriptTitre     let descript_h
+            descriptTitre.innerHTML = product.name;
+            div_description.appendChild(descriptTitre);
 
             //2
             let prix = product.price;
-            let descript_p = document.createElement("p");
-            descript_p.innerHTML = `${cle}`;       //"Vert";
-            div_description.appendChild(descript_p);
-            let descript_2p = document.createElement("p");
-            
-            
-            
-        
+            let descriptCouleur = document.createElement("p");//let descriptCouleur             let descript_p
+            descriptCouleur.innerHTML = `${cle}`;       //"Vert";
+            div_description.appendChild(descriptCouleur);//renommage paragraphe container 
 
-
-            descript_2p.innerHTML = `${valeur}` * prix;//*`${valeur}`= qt
-            div_description.appendChild(descript_2p);
+            let descriptTotalPrix = document.createElement("p");//let descriptTotalPrix       let descript_2p
+            descriptTotalPrix.innerHTML = `${valeur}` * prix;//*`${valeur}`= qt
+            div_description.appendChild(descriptTotalPrix);
             
             let div_settings = document.createElement("div");//
             div_settings.classList.add("cart__item__content__settings");
@@ -86,29 +81,75 @@ fetch('http://localhost:3000/api/products')//je récupére les données
             quanti_input.min = 1;//min max 1 100
             quanti_input.max = 100;//min max 1 100
             quanti_input.value = `${valeur}`;
-            cart_quantiti.appendChild(quanti_input);
-            //console.log(quanti_input.value);//
+            /*====================================
+            ========================================*/
+            quanti_input.addEventListener("change",function(event){
+            console.log(this.value);//ok 5
+            console.log("change",this.value);//ok
+            console.log("change",event,this.value);//faire la fonction ligne 86
+            //console.log("");
+            // 1 new value
+            //descriptTotalPrix.innerHTML = `${valeur}` * prix;
+            //let prix = product.price;
+            //
+            console.log(this.value);//ok 5
+            console.log(prix);//ok 1849
+            let changementValeurInput =this.value;
+            let changementArticle = prix * changementValeurInput;//new
+            console.log(changementArticle);//9245
+            console.log(descriptTotalPrix.innerHTML);//7396
+            descriptTotalPrix.innerHTML =changementArticle;//ok 9245
 
+
+
+            //change 4 et 7396 
+            //div_settings et cart_quantiti
+            /*let totalQt = document.getElementById("totalQuantity");
+            totalQt.innerHTML = SommeQuantite ;
+            let totalPrix = document.getElementById("totalPrice");
+            totalPrix.innerHTML = sommeToutLesArticle;*/
+            
+            
+            let toto = "allo";
+            let titi = "oui";
+            
+            console.log(SommeQuantite);4
+            console.log(sommeToutLesArticle);//7396
+
+            //SommeQuantite = this.value;//5
+            //sommeToutLesArticle = changementArticle;
+            totalQt.innerHTML = this.value;//5
+            totalPrix.innerHTML = descriptTotalPrix.innerHTML;
+
+              //modifier local storage
+              console.log(produitLocalStorage);
+
+
+
+            });
+            /*https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Operators/this*/
+
+
+
+            /*====================================================
+            =====================================================*/
+            cart_quantiti.appendChild(quanti_input);
             let cart_supp = document.createElement("div");
             cart_supp.classList.add("cart__item__content__settings__delete");
             div_settings.appendChild(cart_supp);
-
             //bouton p class deleteItem
             let cart_delet = document.createElement("p");
             cart_delet.classList.add("deleteItem");
-            cart_delet.dataset.id = product._id;//valeur product id
-            cart_delet.dataset.color = `${cle}`;//valeur product id
+            cart_delet.dataset.id = product._id;
+            cart_delet.dataset.color = `${cle}`;
             //console.log(product);
             cart_delet.innerHTML = "Supprimer";
             cart_delet.addEventListener("click", function(e){supprimerArticlePanier(e)});
             cart_supp.appendChild(cart_delet);
-
-      
             //renomer fonction
           function supprimerArticlePanier(e) {
           let panier = JSON.parse(localStorage.getItem("panier"));
-/*{"id": "107fb5b75607497b96722bda5b504926",
-  "color": "Blue"*/
+          /*{"id": "107fb5b75607497b96722bda5b504926","color": "Blue"*/
             console.log(panier);
             delete panier[e.target.dataset.id][e.target.dataset.color];
             console.log(panier);
@@ -116,38 +157,125 @@ fetch('http://localhost:3000/api/products')//je récupére les données
             //closest
             let article = e.target.closest("article");
             article.remove();
-            
-            //le 05 10 2023 13h30
-            sumpx.remove();//suprimer qt article html ok
             }
-          
-            //refactoriser
-            let nbpx = parseInt(descript_2p.innerHTML);
-            totalPx.push(nbpx);
-            let sumpx = 0;
+          //refactoriser
+            //renomer les variable
+            //faire la fonction ligne 86
+            //renommage paragraphe container
+            //RENOMER LES VARIABLES 
+            let prixTotalArticle = parseInt(descriptTotalPrix.innerHTML);//nbpx;//ok 10046€    prixTotalArticle
+            //console.log("prixTotalArticle " + prixTotalArticle);
+            totalPx.push(prixTotalArticle);
+            let sommeToutLesArticle = 0;//sumpx    sommeToutLesArticle
             for (let i = 0; i < totalPx.length; i++) {
-              sumpx += totalPx[i];
+            sommeToutLesArticle += totalPx[i];
             }
-
-            let nbqt = parseInt(quanti_input.value);
-            totalQant.push(nbqt);
-            let sumqt = 0;
+            
+            let sommeNombreTotalArticle = parseInt(quanti_input.value);//valeurInput nbqt    sommeNombreTotalArticle
+            //console.log(sommeNombreTotalArticle);
+            totalQant.push(sommeNombreTotalArticle);
+            let SommeQuantite = 0;//ok 4
             for (let i = 0; i < totalQant.length; i++) {
-              sumqt += totalQant[i];
+            SommeQuantite += totalQant[i];
             }
-
-            //console.log(sumpx);//ok 10046€
-            //console.log(sumqt);//ok 4
+            //console.log(SommePrix);//ok 10046€
+            //console.log(SommeQuantite );//ok 4
             let totalQt = document.getElementById("totalQuantity");
-            totalQt.innerHTML = sumqt;
+            totalQt.innerHTML = SommeQuantite ;
             let totalPrix = document.getElementById("totalPrice");
-            totalPrix.innerHTML = sumpx;
-
+            totalPrix.innerHTML = sommeToutLesArticle;
+            
 
           }//2 for
         });//fetch
     }//1 for
   });//fetch
+
+//regex
+
+
+
+//^reparere envoi
+// requete  POST/order
+/*
+Requête JSON contenant un
+objet de contact et un
+tableau de produitsRetourne l'objet contact, le tableau
+produits et orderId (string)
+*/
+
+
+
+//============================================================
+//envoi api
+/*preparer un objet objet + un tableau post
+fetch('http://localhost:3000/api/order',{on met la code methode + body})  //je récupére les données get
+  .then(data => data.json())//je les appel data et les converties en jason
+  .then(data => {//j'appel mon objet card et lui aplique la fonction qui suit
+console.log(data);
+
+//post
+  });//fetch
+  
+  let order = {
+  contact: {
+    firstName: string,
+    lastName: string,
+    address: string,
+    city: string,
+    email: string
+     },products:[]
+    }*/
+    
+//====================================================================================
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  /* products: [string] <-- array of product _id
+  
+  
   //probleme modif input
   //faire envoi
   //controler ligne 121 supprimer
+//la suite formation
+
+
+
+
+/*La page Confirmation
+
+Une page “confirmation” :
+Un message de confirmation de commande, remerciant l'utilisateur pour sa
+commande, et indiquant l'identifiant de commande envoyé par l’API.
+
+Sur cette page, l'utilisateur doit voir s’afficher son numéro de commande. Il faudra veiller à
+ce que ce numéro ne soit stocké nulle part.
+
+
+Pour les routes POST, l’objet contact envoyé au serveur doit contenir les champs firstName,
+lastName, address, city et email. Le tableau des produits envoyé au back-end doit être un
+array de strings product-ID. Les types de ces champs et leur présence doivent être validés
+avant l’envoi des données au serveur.
+
+requete
+Expects request to contain:
+  contact: {
+    firstName: string,
+    lastName: string,
+    address: string,
+    city: string,
+    email: string
+  }
+  products: [string] <-- array of product _id
+
+*/
